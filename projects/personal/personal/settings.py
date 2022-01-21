7f3ae9715a5be8f5fe803c7a1d512387d5f846b4
Weaponz <weaponz326@gmail.com>
2022-01-21 16:09:46 +0000

    corrected admin credentials

--- projects/personal/personal/settings.py
+++ projects/personal/personal/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--$!lu(#vp07b)e&4+fx!n@l!a2e*)v$ha0228cr2c(gbq-ysb5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG == True:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'personal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG == True:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -115,7 +115,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'personal_db',
-            'USER': 'netrink_admin',
+            'USER': 'personal_admin',
             'PASSWORD': 'admin1r',
             'HOST': 'localhost',
             'PORT': '',
