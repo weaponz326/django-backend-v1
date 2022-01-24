3080485bf54c213f1ced72e252c3b3af8cf3f73e
Weaponz <weaponz326@gmail.com>
2022-01-24 11:13:04 +0000

    nostatic on top of apps

--- projects/personal/personal/settings.py
+++ projects/personal/personal/settings.py
@@ -37,15 +37,14 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
-
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
 
     'corsheaders',
     'django_filters',
