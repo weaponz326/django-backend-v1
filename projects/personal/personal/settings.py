55f57ec7e5bfc162d02d733b9cf2b3fe82eebff1
Weaponz <weaponz326@gmail.com>
2022-01-25 16:40:00 +0000

    .

--- projects/personal/personal/settings.py
+++ projects/personal/personal/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
